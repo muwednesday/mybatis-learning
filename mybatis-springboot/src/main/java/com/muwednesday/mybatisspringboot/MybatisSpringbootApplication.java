bcfa57f018a88f45a555a0225679ba754770aba1
muwednesday <muwednesday@163.com>
2018-08-23 20:26:34 +0800

    Initial commit

---
+++ mybatis-springboot/src/main/java/com/muwednesday/mybatisspringboot/MybatisSpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.muwednesday.mybatisspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatisSpringbootApplication.class, args);
+	}
+}
